90e62ed3f7344f1f97eb879e6dd39d68ad617b18
zack.chen <zack.chen@idreamsky.com>
2019-07-30 11:31:47 +0800

    230_kth smallest element in a bst 自己的实现方式

--- 230_kth smallest element in a bst/kthsmallest.go
+++ 230_kth smallest element in a bst/kthsmallest.go
@@ -14,6 +14,48 @@ func main() {
 	// vals := []int{5, 3, 6, 2, 4, -1, -1, 1}
 	root := common.MakeTree(vals)
 	fmt.Println("Output:", kthSmallest(root, k))
+	fmt.Println("Output:", myKthSmallest(root, k))
+}
+
+func myKthSmallest(root *TreeNode, k int) int {
+	kk = k
+	midOrder(root)
+	return val
+}
+
+var val, kk int
+
+func midOrder(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	midOrder(root.Left)
+	kk--
+	if kk == 0 {
+		val = root.Val
+		return
+	}
+	midOrder(root.Right)
+}
+
+func kthSmallest1(root *TreeNode, k int) int {
+	val := -1
+	midOrder1(root, &k, &val)
+	return val
+}
+
+func midOrder1(root *TreeNode, k *int, val *int) {
+	if root == nil || *k == 0 {
+		return
+	}
+	midOrder1(root.Left, k, val)
+	*k--
+	if *k == 0 {
+		*val = root.Val
+		//val = &root.Val 这种写法是错误的
+		return
+	}
+	midOrder1(root.Right, k, val)
 }
 
 //执行用时 :12 ms, 在所有 Go 提交中击败了99.30%的用户
