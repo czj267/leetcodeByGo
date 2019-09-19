8d8986030d2cb2f94ca77fb4a92ae43041fe8169
zack.chen <zack.chen@idreamsky.com>
2019-09-19 15:24:21 +0800

    令牌桶算法

--- 4_median of two sorted arrays/findmediansortedarrays.go
+++ 4_median of two sorted arrays/findmediansortedarrays.go
@@ -11,6 +11,23 @@ func main() {
 	fmt.Println("Output:", findMedianSortedArrays(nums1, nums2))
 }
 
+func findMedianSortedArrays1(nums1 []int, nums2 []int) float64 {
+	l1 := len(nums1)
+	l2 := len(nums2)
+	m1 := l1 / 2
+	m2 := l2 / 2
+	if nums1[m1] == nums2[m2] {
+		return float64(nums1[m1])
+	}
+
+	if nums1[m1] > nums2[m2] {
+
+	} else {
+
+	}
+	return 0.1
+}
+
 //执行用时 :16 ms, 在所有Go提交中击败了97.95%的用户
 //内存消耗 :6 MB, 在所有Go提交中击败了27.06%的用户
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
